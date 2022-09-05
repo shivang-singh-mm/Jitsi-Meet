7bd84842bebfba723f835d18b4b7ce311031a466
Shivang Singh <shivangsingh2240@gmail.com>
2022-09-05 14:17:19 +0530

    jitsi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import JitsiComponent from './jitsi';
 import './App.css';
+// import VideoConference from './new.j';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>hello !</h1>
+    {/* < VideoConference /> */}
+     <JitsiComponent /> 
+    </>
   );
 }
 
